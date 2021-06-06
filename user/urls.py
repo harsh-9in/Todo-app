d7c4ef5ce16a02ed537c69f98d92f673ee652067
harsh-9in <harshkumar9.in@gmail.com>
2021-06-06 21:13:38 +0530

    feat: created authentication endpoints

---
+++ user/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = DefaultRouter()
+router.register('signup',views.UserProfileViewset)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from . import models
+from . import serializers
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated,]
+
+
+class UserLogin(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
